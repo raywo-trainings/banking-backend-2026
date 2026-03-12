0ee3d462fda019f28e3a0e46c24297c920e7f59f
Ray Wojciechowski <ray.wojciechowski@ars.de>
2026-03-12 12:24:02 +0100

    Initial commit

---
+++ src/test/java/de/raywo/banking/bankingbackend/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.raywo.banking.bankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
