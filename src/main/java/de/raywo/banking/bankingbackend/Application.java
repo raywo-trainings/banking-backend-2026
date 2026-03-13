a631535f08c6ff91ed8f62bcaef99dcdaf649187
Ray Wojciechowski <ray.wojciechowski@ars.de>
2026-03-13 08:44:47 +0100

    Add customer controller implementation

--- src/main/java/de/raywo/banking/bankingbackend/Application.java
+++ src/main/java/de/raywo/banking/bankingbackend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankingBackendApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(BankingBackendApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
---
+++ src/main/java/de/raywo/banking/bankingbackend/boundary/customers/CustomersController.java
@@ -0,0 +1,106 @@
+package de.raywo.banking.bankingbackend.boundary.customers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/customers")
+public class CustomersController {
+
+  private final Map<String, CustomerDTO> customers = new HashMap<>();
+
+
+  public CustomersController() {
+    String id1 = generateId();
+    customers.put(id1, new CustomerDTO(id1, "John Doe", "New York"));
+    String id2 = generateId();
+    customers.put(id2, new CustomerDTO(id2, "Jane Smith", "Los Angeles"));
+  }
+
+
+  @GetMapping()
+  public Collection<CustomerDTO> getCustomers() {
+    return customers.values();
+  }
+
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerDTO> getCustomer(@PathVariable String id) {
+    if (!customers.containsKey(id)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(customers.get(id));
+  }
+
+
+  @PostMapping
+  public ResponseEntity<CustomerDTO> createCustomer(
+      @RequestBody CustomerDTO customer,
+      UriComponentsBuilder uriBuilder
+  ) {
+    final String id = generateId();
+    CustomerDTO newCustomer = new CustomerDTO(
+        id,
+        customer.getName(),
+        customer.getCity()
+    );
+    customers.put(id, newCustomer);
+
+    URI location = uriBuilder
+        .path("/{id}")
+        .buildAndExpand(id)
+        .toUri();
+
+    return ResponseEntity
+        .created(location)
+        .body(newCustomer);
+  }
+
+
+  @PutMapping("/{id}")
+  public ResponseEntity<CustomerDTO> updateCustomer(
+      @PathVariable String id,
+      @RequestBody CustomerDTO customer
+  ) {
+    if (!customers.containsKey(id)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    CustomerDTO updatedCustomer = new CustomerDTO(
+        id,
+        customer.getName(),
+        customer.getCity()
+    );
+
+    customers.put(id, updatedCustomer);
+
+    return ResponseEntity.ok(updatedCustomer);
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteCustomer(@PathVariable String id) {
+    customers.remove(id);
+
+    return ResponseEntity.noContent().build();
+  }
+
+
+  private String generateId() {
+    return customers.keySet()
+        .stream()
+        .map(Integer::parseInt)
+        .max(Comparator.naturalOrder())
+        .map(n -> n + 1)
+        .map(String::valueOf)
+        .orElse("0");
+  }
+
+}
